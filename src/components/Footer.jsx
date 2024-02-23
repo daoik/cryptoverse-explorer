524ae5391903941198b7b7c54f105e488906f1b3
daoik <dimeconomu@gmail.com>
2024-02-24 01:23:13 +0200

    footer stuff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex w-full justify-center items-center">
           <div className="flex flex-col w-full justify-end items-center">
-            {/* <h3 className="text-lg font-bold mb-4">About</h3> */}
             <p>
               Made with ❤️ by{" "}
               <a href="https://github.com/daoik">Dimitris Oikonomou </a>
@@ -16,26 +15,6 @@ const Footer = () => {
               API
             </p>
           </div>
-          {/* <div className="col-span-1">
-            <h3 className="text-lg font-bold mb-4">Contact Us</h3>
-            <p>Email: example@example.com</p>
-            <p>Phone: 123-456-7890</p>
-            <p>Address: 123 Main St, City, Country</p>
-          </div>
-          <div className="col-span-1">
-            <h3 className="text-lg font-bold mb-4">Follow Us</h3>
-            <div className="flex space-x-4">
-              <a href="#" className="text-gray-300 hover:text-white">
-                Facebook
-              </a>
-              <a href="#" className="text-gray-300 hover:text-white">
-                Twitter
-              </a>
-              <a href="#" className="text-gray-300 hover:text-white">
-                Instagram
-              </a> 
-            </div>
-          </div>*/}
         </div>
       </div>
     </footer>
