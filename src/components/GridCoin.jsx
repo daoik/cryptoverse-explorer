2ecc68bf8264f5dc6cae727df7708ff35e84cd28
daoik <dimeconomu@gmail.com>
2024-03-28 22:26:34 +0200

    clean up

--- src/components/GridCoin.jsx
+++ src/components/GridCoin.jsx
@@ -9,16 +9,11 @@ import { fetchCoinData } from "./api";
 const CardCoin = ({ coin }) => {
   const [coinData, setCoinData] = useState({});
   const navigate = useNavigate();
-  const handeCardClick = (coin) => {
+  const handeCardClick = () => {
     navigate(`/cryptoverse-explorer/coins/${coinData.id}`);
   };
-  // useEffect(() => {
-  //   fetchCoinData(coin)
-  //     .then(setCoinData)
-  //     .catch((error) => console.error("Error fetching crypto data:", error));
-  // }, [coin]);
+
   useEffect(() => {
-    console.log(coin);
     setCoinData(coin);
   }, []);
 
@@ -26,21 +21,24 @@ const CardCoin = ({ coin }) => {
     coinData && (
       <div
         onClick={handeCardClick}
-        className="p-3 w-72  m-3 self-center transform-all  cursor-pointer hover:outline outline-2 outline-purple-900 hover:bg-zinc-100 rounded-lg shadow-md bg-zinc-200 dark:bg-gray-600 dark:hover:bg-zinc-800 flex flex-col"
+        className="p-3 w-72 h-32  m-3 self-center transform-all  cursor-pointer hover:outline outline-2 outline-purple-900 hover:bg-zinc-100 rounded-lg shadow-md bg-zinc-200 dark:bg-gray-600 dark:hover:bg-zinc-800 flex flex-col"
       >
-        <h3 className="text-xl  inline-flex  w-full justify-between items-center font-semibold mb-4 text-orange-700 dark:text-orange-300">
-          <img className="h-6 pe-2" src={coinData.image} /> {coinData.name}{" "}
-          <span className="inline-block text-gray-700 dark:text-gray-300 px-2 py-1 text-xs font-semibold uppercase">
-            {coinData.symbol}
-          </span>
-          <FavoriteButton className="px-2" id={coin} />
-        </h3>
-        <div className="inline-flex ">
+        {" "}
+        <div className="flex-1 flex items-center">
+          <h3 className="text-xl  inline-flex  w-full justify-between items-center font-semibold mb-4 text-orange-700 dark:text-orange-300">
+            <img className="h-6 pe-2" src={coinData.image} /> {coinData.name}{" "}
+            <span className="inline-block text-gray-700 dark:text-gray-300 px-2 py-1 text-xs font-semibold uppercase">
+              {coinData.symbol}
+            </span>
+            <FavoriteButton className="px-2" id={coinData.id} />
+          </h3>
+        </div>
+        <div className="inline-flex mt-auto">
           <div className="text-3xl">
             ${addCommasToNumber(coinData?.current_price)}
           </div>
           <div
-            className={`px-4 py-2 inline-flex items-center text-end ${
+            className={`px-4 py-2 inline-flex items-center text-end ml-auto ${
               coinData?.price_change_percentage_24h >= 0
                 ? "text-green-600"
                 : "text-red-600"
@@ -54,12 +52,6 @@ const CardCoin = ({ coin }) => {
             {coinData?.price_change_percentage_24h?.toFixed(2)}%
           </div>
         </div>
-
-        {/* Adjust this line according to the structure of your API response */}
-        {/* <p className="text-gray-700 mb-4 dark:text-white">{body}</p> */}
-        {/* <button className="bg-teal-500 hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded dark:bg-teal-700 dark:hover:bg-teal-600 mt-auto">
-        {button}
-      </button> */}
       </div>
     )
   );
