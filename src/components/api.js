a89d7b40c039ea02e1c43155b2646c6aca81064c
daoik <dimeconomu@gmail.com>
2024-03-28 22:14:32 +0200

    removed unnecesary code

--- src/components/api.js
+++ src/components/api.js
@@ -1,17 +1,7 @@
 const APIKEY = import.meta.env.VITE_GECKO_API_KEY;
 
 const api = () => {};
-export const fetchData = async () => {
-  try {
-    const response = await fetch(
-      `https://api.coingecko.com/api/v3/search?query=${searchQuery}&x_cg_demo_api_key=${APIKEY}`
-    );
-    const data = await response.json();
-    setFilteredResults(data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-};
+
 export const fetchCryptoData = async (itemsPerPage, page) => {
   try {
     const response = await fetch(
