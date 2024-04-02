dddba9778615a977973f1db17b2df9f648baca55
daoik <dimeconomu@gmail.com>
2024-04-02 15:24:42 +0300

    404 stylizing

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,12 +7,20 @@ const NotFoundPage = () => {
       <div className="text-xl opacity-80 mt-4  inline-flex items-center space-x-5">
         Cryptoverse <AiOutlineCompass className="mx-2" /> Explorer
       </div>
-      <div className="m-10 moon-bg ] text-start">
-        <div className="inline-flex w-full ">
-          <div className="favorites w-full items-center  justify-center flex-col">
-            <h2 className="text-3xl font-semibold text-center mb-4">
-              Error 404: Page Not Found
+      <div className="m-10 relative text-start flex flex-col">
+        <div className="inline-flex w-full mt-52">
+          <div className="favorites w-full items-center text-center justify-center flex-col">
+            <h2 className="text-3xl font-semibold text-center mb-10">
+              Page Not Found
             </h2>
+            <p className="text-lg mt-10 text-gray-600 dark:text-gray-300">
+              Oops! Looks like you've stumbled upon a page that doesn't exist.
+            </p>
+          </div>
+        </div>
+        <div className="w-full  absolute scale-y-150 scale-x-110 z-20 top-3 opacity-25 mt-52">
+          <div className="favorites w-full items-center text-center justify-center flex-col">
+            <h2 className="text-9xl blur font-bold  text-center mb-4">404</h2>
           </div>
         </div>
       </div>
